40b4ec4a8c48b24bd7804c3ec446cef06d1ce49e
Anthony Marshall <anthony.marshall@wearesuburb.com>
2019-07-04 23:27:29 +1200

    refactor comments

--- graph-searching/breadth-first-search/index.ts
+++ graph-searching/breadth-first-search/index.ts
@@ -7,7 +7,7 @@ const input: Input = Input.parseFile('breadth-first-search');
 //build Graph
 const graph: Graph = new Graph(input.nodePairs);
 
-//traverse graph using dfs
+//traverse graph using bfs
 let result: Number[] = [];
 console.log(graph.nodes)
 traverseBFS(graph.nodes[0], result);
@@ -24,7 +24,7 @@ function traverseBFS(startNode: Node, result: Number[]) {
             queue.push(node);
         })
         if (result.indexOf(currentNode.num) == -1) result.push(currentNode.num);
-        
+
         queue.splice(0, 1);
     }
 
